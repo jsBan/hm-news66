f6841bde3d8cb9ec665bc8ffb7ac48f48912356a
banguoxiang <1786295283@qq.com>
2020-06-30 21:48:24 +0800

    完成了基础的配置

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div class="app"></div>
+    <van-button type="info">信息按钮</van-button>
+    <van-search v-model="value" input-align="center" placeholder="请输入搜索关键词" />
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      value: ''
     }
   }
+}
+</script>
+<style lang="less">
+
+.app {
+  width: 300px;
+  height: 300px;
+  font-size: 75px;
+  background-color: #f60;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import 'amfe-flexible'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+// Vue.use(Vant)
 
+import { Button, Search } from 'vant'
+Vue.use(Button)
+Vue.use(Search)
 Vue.config.productionTip = false
-
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
